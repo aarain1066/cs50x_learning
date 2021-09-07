ff760ddeafbf630557e569d47ba01fb3351b015c
aarain1066 <aarain1066@gmail.com>
2021-09-07 13:15:40 +0000

    Completed gray_scale func

--- week4/pset4/filter/helpers.c
+++ week4/pset4/filter/helpers.c
@@ -1,8 +1,30 @@
 #include "helpers.h"
+#include <math.h>
+
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++){
+
+        for(int j = 0; j < width; j++){
+
+            // Create the number that we wil use to make the gray scaler number
+
+            // this is done by taking the RBG values (Note that in bmp's it's BGR) and averaging them
+
+            int gray_avg = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+
+            // Set to the new average.
+
+            image[i][j].rgbtBlue = gray_avg;
+            image[i][j].rgbtGreen = gray_avg;
+            image[i][j].rgbtRed = gray_avg;
+
+
+
+        }
+    }
     return;
 }
 
