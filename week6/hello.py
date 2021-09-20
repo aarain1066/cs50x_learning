6cf5f7b7732b54632d7b6059f221319a747ef393
aarain1066 <aarain1066@gmail.com>
2021-09-20 15:51:29 +0000

    init commit with blur.py

---
+++ week6/hello.py
@@ -0,0 +1,13 @@
+from cs50 import get_string
+
+
+# Here, we can see a similar thing we did a lot in C
+
+#answer = get_string(" What's youre name? ")
+#
+#print("hello, " + answer)
+
+# Or, we can do the same here
+
+answer = get_string(" What's youre name? ")
+print(f"hello, {answer}")
